06a4c814fd3475ddca8c3a3c12b8831840e3d0f1
OmarFattahi <omarfatahidev@gmail.com>
2023-06-04 18:06:12 +0330

    create category model

--- category/models.py
+++ category/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class ParentCategory(models.Model):
+    title = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    parent = models.ForeignKey(ParentCategory, on_delete=models.CASCADE)
+    title = models.CharField(max_length=30, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.title
