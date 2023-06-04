0f986569c1e87f1bf5680c4bf0fb4e711ef8fc48
OmarFattahi <omarfatahidev@gmail.com>
2023-06-05 00:18:00 +0330

    updated

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('tinymce/', include('tinymce.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
