cd99c4f16b44e5db5e137227d966d4b50b607c8a
OmarFattahi <omarfatahidev@gmail.com>
2023-06-05 01:00:26 +0330

    start html layout. site configure to layout.html

--- config/views.py
+++ config/views.py
@@ -1,1 +1,10 @@
 from django.shortcuts import render
+from .models import Configuration
+
+
+def layout(request):
+    config = Configuration.objects.all()
+    context = {'site': config}
+    template_name = 'layout.html'
+
+    return render(request, template_name, context)
