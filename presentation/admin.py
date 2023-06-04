81a1e9070ced0afcd9459eaf7ba3151fc3ebffa8
OmarFattahi <omarfatahidev@gmail.com>
2023-06-04 22:05:29 +0330

    create model and add to panel

--- presentation/admin.py
+++ presentation/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Presentation
 
-# Register your models here.
+
+@admin.register(Presentation)
+class PresentationAdmin(admin.ModelAdmin):
+        list_disply = ['title', 'slug', 'lock']
+        search_list = ['title', 'content']
--- presentation/apps.py
+++ presentation/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class PresentationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'presentation'
+    verbose_name = "Presentation's"
