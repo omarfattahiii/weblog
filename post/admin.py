101598546e2a450b1c9f49e3fcab87b6dda35e71
OmarFattahi <omarfatahidev@gmail.com>
2023-06-04 18:06:42 +0330

    create post model and add to admin

--- post/admin.py
+++ post/admin.py
@@ -1,1 +1,8 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
+from .models import SinglePost
+
+class SinglePostContent(SummernoteModelAdmin):
+    summernote_fields = ('content',)
+
+admin.site.register(SinglePost, SinglePostContent)
--- post/models.py
+++ post/models.py
@@ -1,1 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
+from category.models import Category
+
+
+class SinglePost(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    up_date = models.DateTimeField(auto_now=True)
+    # TODO: comment field
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    lock = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
