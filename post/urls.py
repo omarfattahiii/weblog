208af4dc58cd79389db2180ea91e8e4285422988
OmarFattahi <omarfatahidev@gmail.com>
2023-06-05 15:26:29 +0330

    create single and serie details post, main post page

--- post/urls.py
+++ post/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 app_name = "post"
 urlpatterns = [
+    path('', views.posts, name="posts"),
     path('single/', views.single_posts, name="single_posts"),
+    path('single/<int:pk>/', views.single_posts_detail, name="single_posts_detail"),
     path('serie/', views.serie_posts, name="serie_posts"),
+    path('serie/<int:pk>/', views.serie_posts_detail, name="serie_posts_detail"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import SinglePost, SeriePost
+
+def posts(request):
+    single = SinglePost.objects.all().order_by("up_date", "pub_date")
+    serie = SeriePost.objects.all().order_by("up_date", "pub_date")
+    context = {'single': single, 'serie': serie}
+    template_name = 'post/posts.html'
+
+    return render(request, template_name, context)
 
 def single_posts(request):
     objects = SinglePost.objects.all().order_by("up_date", "pub_date")
@@ -8,9 +16,23 @@ def single_posts(request):
 
     return render(request, template_name, context)
 
+def single_posts_detail(request, pk):
+    object = get_object_or_404(SinglePost, id=pk)
+    context = {'object': object}
+    template_name = 'post/single_posts_detail.html'
+
+    return render(request, template_name, context)
+
 def serie_posts(request):
     objects = SeriePost.objects.all().order_by("up_date", "pub_date")
     context = {'objects': objects}
     template_name = 'post/serie_posts.html'
+
+    return render(request, template_name, context)
+
+def serie_posts_detail(request, pk):
+    object = get_object_or_404(SeriePost, id=pk)
+    context = {'object': object}
+    template_name = 'post/serie_posts_detail.html'
 
     return render(request, template_name, context)
