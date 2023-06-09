19efda2fba443e6b6af33778c9209258738118be
OmarFattahi <omarfatahidev@gmail.com>
2023-06-10 03:18:20 +0330

    responsive single post detail

--- post/templatetags/post.py
+++ post/templatetags/post.py
@@ -6,7 +6,7 @@ register = template.Library()
 @register.inclusion_tag("post/partials/last_single_post.html")
 def last_single_post():
     try:
-        single = SinglePost.objects.filter(is_published=True).order_by("-up_date", "-pub_date")[:5]
+        single = SinglePost.objects.filter(is_published=True).order_by("-pub_date")[:5]
 
         return {'single': single}
     except Exception:
@@ -15,7 +15,7 @@ def last_single_post():
 @register.inclusion_tag("post/partials/last_serie_post.html")
 def last_serie_post():
     try:
-        serie = SeriePost.objects.filter(is_published=True).order_by("-up_date", "-pub_date")[:5]
+        serie = SeriePost.objects.filter(is_published=True).order_by("-pub_date")[:5]
 
         return {'serie': serie}
     except Exception:
